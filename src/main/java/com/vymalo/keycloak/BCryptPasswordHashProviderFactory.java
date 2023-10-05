9e651867a34be630eaa4310da151c51b64cc53b6
belocerkovsky <belocerkovsky@safe-line.ru>
2023-10-05 15:14:18 +0300

    micro fix

--- src/main/java/com/vymalo/keycloak/BCryptPasswordHashProviderFactory.java
+++ src/main/java/com/vymalo/keycloak/BCryptPasswordHashProviderFactory.java
@@ -14,6 +14,7 @@ import java.util.Map;
  * @author <a href="mailto:pro.guillaume.leroy@gmail.com">Guillaume Leroy</a>
  */
 public class BCryptPasswordHashProviderFactory implements PasswordHashProviderFactory, ServerInfoAwareProviderFactory {
+
     public static final String ID = "bcrypt";
     public static final int DEFAULT_ITERATIONS = 8192;
 
